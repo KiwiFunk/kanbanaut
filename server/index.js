12a24e96fe14ff1ceea64964e516eb6f237b488e
Alex <alex_p97@live.com>
2025-03-04 12:39:02 +0000

    Linked MongoDB database, initial project setup complete

--- server/index.js
+++ server/index.js
@@ -18,3 +18,12 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Error connecting to MongoDB', err);
+});
