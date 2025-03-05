feccdae93452bca53df0b25914b7e1942ef5d18d
Alex <alex_p97@live.com>
2025-03-05 17:45:46 +0000

    Updated name inside of header h1

--- client/src/Header.js
+++ client/src/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = ({ onLogout }) => {
   return (
     <div className="header-container">
-      <h1>Kanban Board</h1>
+      <h1>Kanbanaut</h1>
       <button className="logout-button" onClick={onLogout}>Logout</button>
     </div>
   );
