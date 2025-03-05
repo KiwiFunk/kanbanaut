34f6594f632f116f90d8a5b8baffc3bcc758da8e
Alex <alex_p97@live.com>
2025-03-05 11:56:29 +0000

    Updated Todo model with new fields

--- server/models/Todo.js
+++ server/models/Todo.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 const TodoSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    completed: { type: Boolean, default: false },
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, 
+    title: { type: String, required: true },                                                    //Title of our issue
+    body: { type: String, default: "" },                                                        //Description of our issue
+    status: { type: String, enum: ['todo', 'inprogress', 'complete'], default: 'todo' },        //Status of our issue
+    completed: { type: Boolean, default: false },                                               //Is the issue completed
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },              //User who created the issue
 });
 
 module.exports = mongoose.model('Todo', TodoSchema);
